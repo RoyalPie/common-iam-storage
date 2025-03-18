8e3e46f2ba3a7173d5f42fd105bd8303e1546e7f
Truong.TK <truongtk1711@gmail.com>
2025-03-18 10:10:17 +0700

    gateway and eureka fixed

---
+++ service-registry/src/test/java/com/evo/service_registry/ServiceRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.evo.service_registry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
